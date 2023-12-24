9e49833fc1eecd6ae171885491e5bdb3277dce9c
priyankasaini75 <153932362+priyankasaini75@users.noreply.github.com>
2023-12-24 05:56:35 -0800

    added sphere

--- hw3d/App.h
+++ hw3d/App.h
@@ -33,14 +33,14 @@ private:
 	float speed_factor = 1.0f;
 	CameraContainer cameras;
 	PointLight light;
-	TestCube cube{ wnd.Gfx(),4.0f };
-	SolidSphere cube2{ wnd.Gfx(),4.0f };
+	TestCube cube{ wnd.Gfx(),4.0f,""}; 
 
 	//TestCube cube2{ wnd.Gfx(),4.0f };
 	//Model sponza{ wnd.Gfx(),"Models\\sponza\\sponza.obj",1.0f / 20.0f };
 	//Model gobber{ wnd.Gfx(),"Models\\gobber\\GoblinX.obj",4.0f };
 	Model gobber{ wnd.Gfx(),"Models\\teapot\\teapot.obj",0.05f };
-	Model nano{ wnd.Gfx(),"Models\\nano_textured\\nanosuit.obj",2.0f };
+	Model nano{ wnd.Gfx(),"Models\\wooden_sphere\\wooden_sphere.obj",0.5f };
+	//Model nano{ wnd.Gfx(),"Models\\nano_textured\\nanosuit.obj",2.0f };
 
 	bool savingDepth = false;
 };
